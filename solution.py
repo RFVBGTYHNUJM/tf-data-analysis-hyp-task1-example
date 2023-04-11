1dbe017a815d4c8d444ca0d493253dad3d6271be
RFVBGTYHNUJM <129897748+RFVBGTYHNUJM@users.noreply.github.com>
2023-04-11 17:56:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,22 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    alpha = 0.01
+    p0 = x_success / x_cnt
+    p1 = y_success / y_cnt
+    f0 = norm.cdf(np.sqrt(x_cnt) 
+                    * (p0 - alpha)
+                    / np.sqrt(p0 * (1 - p0)))
+    f1 = norm.cdf(np.sqrt(y_cnt)
+                    * (p1 - alpha)
+                    / np.sqrt(p1 * (1 - p1)))
+
+    if f1 > f0 :
+        f = 1
+    else :
+        f = 0
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return f # Ваш ответ, True или False
